09ca2b48827a869c0d797ff181cec6840868e0d4
I. Sautkin <iosautkin@gmail.com>
2022-04-25 18:14:17 +0700

    пофиксил синхронизацию

--- src/pages/Events/EventsPage.tsx
+++ src/pages/Events/EventsPage.tsx
@@ -3,6 +3,7 @@ import { observer } from 'mobx-react-lite';
 import { Calendar, Typography } from 'antd';
 import { useSearchParams } from 'react-router-dom';
 import moment from 'moment';
+import { CalendarMode } from 'antd/lib/calendar/generateCalendar';
 // import { Link } from 'react-router-dom';
 // import { LOGOUT } from '../../routes';
 // import { gql, useQuery } from '@apollo/client';
@@ -24,16 +25,26 @@ import moment from 'moment';
 const EventsPage: FC = () => {
   // const { loading, error, data } = useQuery(GET_ME, {fetchPolicy: 'network-only'});
   const [params, setSearchParams] = useSearchParams();
-  const date = params.get('date') || moment();
+  const date = params.get('date');
+  const mode: CalendarMode = (params.get('mode') as any) || 'month';
+
+  function onChangeDate(dateNew: moment.Moment, modeNew: CalendarMode = 'month') {
+    const dateString = dateNew.format('YYYY-MM');
+    console.log(dateNew, modeNew);
+    if (date !== dateString || mode !== modeNew) {
+      setSearchParams(`date=${dateString}&mode=${modeNew}`);
+    }
+  }
 
   return (
     <div style={{ padding: 60 }}>
       <Typography.Title>Events Page</Typography.Title>
 
       <Calendar
-        validRange={[moment('2021-04-01'), moment('2099-01-01')]}
+        validRange={[moment('2021-04'), moment('2099-01')]}
         defaultValue={moment(date)}
-        onChange={v => setSearchParams(`date=${v.format('YYYY-MM-DD')}`)}
+        mode={mode}
+        onPanelChange={onChangeDate}
       />
     </div>
   )
