b40fd40dcc1984daf8d0e5d02550a47acf1223d9
I. Sautkin <iosautkin@gmail.com>
2022-04-25 17:04:59 +0700

    удалил лишний код

--- src/pages/Events/EventsPage.tsx
+++ src/pages/Events/EventsPage.tsx
@@ -1,26 +1,26 @@
 import React, { FC } from 'react';
 import { observer } from 'mobx-react-lite';
 import { Calendar, Typography } from 'antd';
-import { Link } from 'react-router-dom';
-import { LOGOUT } from '../../routes';
-import { gql, useQuery } from '@apollo/client';
+// import { Link } from 'react-router-dom';
+// import { LOGOUT } from '../../routes';
+// import { gql, useQuery } from '@apollo/client';
 
-const GET_ME = gql`
-    query getMe {
-        me {
-            id
-            email
-            username
-            role {
-                id
-                name
-            }
-        }
-    }
-`;
+// const GET_ME = gql`
+//     query getMe {
+//         me {
+//             id
+//             email
+//             username
+//             role {
+//                 id
+//                 name
+//             }
+//         }
+//     }
+// `;
 
 const EventsPage: FC = () => {
-  const { loading, error, data } = useQuery(GET_ME, {fetchPolicy: 'network-only'});
+  // const { loading, error, data } = useQuery(GET_ME, {fetchPolicy: 'network-only'});
 
   return (
     <div style={{ padding: 60 }}>
